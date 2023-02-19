ea3cde8d803a23b67c0561d5207c043be568e9c6
kulwinder2510 <kulwinder9053@gmail.com>
2023-02-18 19:10:10 -0500

    commit

---
+++ src/Student.java
@@ -0,0 +1,12 @@
+public class Student {
+    String name;
+    String phoneNumber;
+    int math;
+    int sci;
+    int chem;
+
+    void Studentinfo(){
+          System.out.println("my name is : " + name + " , my phone number is : " + phoneNumber + " and i have got marks in math :" + math + " , science :" + sci + " , chem :" + chem);
+      }
+}
+
