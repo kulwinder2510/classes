619a83a7575820ff8567592a691e7fa99e244b11
kulwinder2510 <kulwinder9053@gmail.com>
2023-02-18 19:10:43 -0500

    commit

---
+++ src/Book.java
@@ -0,0 +1,13 @@
+//public class Book {
+//    String color;
+//    String name;
+//    int page;
+//    char dimension;
+//    double lines;
+//
+//    public void BookQuality(){
+//        System.out.println("i have red color Dinosaur book and have 250 pages and diagrams also.");
+//
+//    }
+//
+//}
